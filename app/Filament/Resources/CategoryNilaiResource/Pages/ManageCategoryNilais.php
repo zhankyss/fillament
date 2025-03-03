c0865f424bf887933bd789441bb8c3b1ee06eb9d
zhankyss <akunprojectzhkys@gmail.com>
2025-03-03 10:18:40 +0700

    cara merubah title dan label

--- app/Filament/Resources/CategoryNilaiResource/Pages/ManageCategoryNilais.php
+++ app/Filament/Resources/CategoryNilaiResource/Pages/ManageCategoryNilais.php
@@ -16,4 +16,9 @@ class ManageCategoryNilais extends ManageRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    public function getTitle(): string
+    {
+        return 'Category Nilai';
+    }
 }
