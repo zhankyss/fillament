0ded4e7dc853689fedd02ea538b8af02bd8949c2
zhankyss <akunprojectzhkys@gmail.com>
2025-03-06 09:47:45 +0700

    cara menngunakan theme di fillamrnt

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -66,6 +66,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->viteTheme('resources/css/filament/admin/theme.css')
+            ;
     }
 }
